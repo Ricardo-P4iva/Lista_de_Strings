15f4b7705077415e54559f05863fe79bf9917409
aluno <ricardo.paivalmeida@gmail.com>
2024-07-30 22:22:25 -0300

    lista de Strings

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        //Exercicio 1: contar caracteres
+        System.out.println("digite um texto");
+
+        String texto = entrada.nextLine();
+//        System.out.println(texto.length());
+//
+//        //Exercicio 3: contar vogais
+//
+//        System.out.println("digite uma palavra");
+//
+//        String vogais = entrada.nextLine();
+//            int cont = 0;
+//        for (int i = 0; i < vogais.length(); i++)
+//        {
+//            if (vogais.charAt(i) == 'a' || vogais.charAt(i) == 'e' || vogais.charAt(i) == 'i'  || vogais.charAt(i) == 'o' || vogais.charAt(i) == 'u')
+//            {
+//                cont++;
+//            }
+//        }
+//        System.out.println(cont);
+
+//        Exercicio 4: remover espaços
+//
+//        String semEspaco = texto.replace(" ","");
+//        System.out.println(semEspaco);
+
+        //Exercicio 5: substituir caracteres
+
+//        String substituir = texto.replace(" ", "8");
+//        System.out.println(substituir);
+
+        //Exercicio 2: palindromo
+
+
+
+    }
+}
